53f5c5fa985c72408eb66e5dd26e7eac5b8830bb
Urban20 <matiasurbaneja@hotmail.com.ar>
2024-08-19 14:47:04 -0300

    Add files via upload

---
+++ autoclick.py
@@ -0,0 +1,51 @@
+import pyautogui,keyboard
+
+
+
+   
+print(r'''
+
+      _   _____  _____  _________    ___              ______  _____     _____   ______  ___  ____   
+     / \ |_   _||_   _||  _   _  | .'   `.          .' ___  ||_   _|   |_   _|.' ___  ||_  ||_  _|  
+    / _ \  | |    | |  |_/ | | \_|/  .-.  \ ______ / .'   \_|  | |       | | / .'   \_|  | |_/ /    
+   / ___ \ | '    ' |      | |    | |   | ||______|| |         | |   _   | | | |         |  __'.    
+ _/ /   \ \_\ \__/ /      _| |_   \  `-'  /        \ `.___.'\ _| |__/ | _| |_\ `.___.'\ _| |  \ \_  
+|____| |____|`.__.'      |_____|   `.___.'          `.____ .'|________||_____|`.____ .'|____||____| ''')
+                                                                                                    
+print(r'''
+MMMMMMMO:oXMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMk. 'oXMMMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMk.   'dXMMMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMk.     'dXMMMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMk.       'dXMMMMMMMMMMMMMMMMMMMMM
+MMMMMMMk.         'x0XMMMMMMMMMMMMMMMMMM
+MMMMMMMk.           .,xXMMMMMMMMMMMMMMMM
+MMMMMMMk.              ,xNMMMMMMMMMMMMMM
+MMMMMMMk.                ,xNMMMMMMMMMMMM
+MMMMMMMk.                  ,kNMMMMMMMMMM
+MMMMMMMk.                    ;kNMMMMMMMM
+MMMMMMMk.              .'',,;;l0WMMMMMMM
+MMMMMMMk.              ;0NWWWWMMMMMMMMMM
+MMMMMMMk.    .,.        lNMMMMMMMMMMMMMM
+MMMMMMMk.  ,dXNl        .dWMMMMMMMMMMMMM
+MMMMMMMO,:kNMMMX:        .kWMMMMMMMMMMMM
+MMMMMMMNXWMMMMMM0,        'OMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMWO'        ;KMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMWx.    .,cxXMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMXc..,oONMMMMMMMMMMMMMM
+''')
+print('por Urb@n')
+def ejecucion():
+        try:
+            while not keyboard.is_pressed('-'):
+                pyautogui.mouseDown()
+                if keyboard.is_pressed('1'):
+                    keyboard.press('w')
+        except:
+            escuchando()
+
+def escuchando():
+    keyboard.add_hotkey('|',ejecucion)
+    keyboard.wait()
+    
+escuchando()
